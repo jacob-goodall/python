1e4214c6788aee27653d7d044dbedad92fcbbe18
jacob-goodall <63982530+jacob-goodall@users.noreply.github.com>
2021-05-31 11:59:01 +1200

    Thingys

--- Code/age_to_sec_convert.py
+++ Code/age_to_sec_convert.py
@@ -1,12 +1,15 @@
 # Figure out your age
 import datetime
 currentDate = datetime.datetime.now()
+#gets current date and time
 
 deadline = input ('Please enter your date of birth (dd/mm/yyyy): ')
 deadlineDate = datetime.datetime.strptime(deadline,'%d/%m/%Y')
+#removes time from the input
 print(deadlineDate)
 daysLeft = deadlineDate - currentDate
 daysLeft = abs(daysLeft)
+#makes it to a postive number
 print(daysLeft)
 
 years = ((daysLeft.total_seconds())/(365.242*24*3600))
@@ -32,7 +35,6 @@ minutesInt = int(minutes)
 seconds = (minutes-minutesInt)*60
 seconds = abs(seconds)
 secondsInt = int(seconds)
-
 
 print('You are {0:d} years, {1:d}  months, {2:d}  days, {3:d}  hours, {4:d} \
  minutes, {5:d} seconds old.'.format(yearsInt,monthsInt,daysInt,hoursInt,minutesInt,secondsInt))
