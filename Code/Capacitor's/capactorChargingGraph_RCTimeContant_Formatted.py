06d3f20b0dbd632188ec8371d58c80d2442cad80
jacob-goodall <63982530+jacob-goodall@users.noreply.github.com>
2021-06-10 19:45:30 +1200

    Added Error Check and Prevention SI unit prefix conversion eg micro farats T = RC to make program run only till 5RC
    Todo -
    Add Selectable graph data

--- Code/Capacitor's/capactorChargingGraph_RCTimeContant_Formatted.py
+++ Code/Capacitor's/capactorChargingGraph_RCTimeContant_Formatted.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+
 # Not needed to make graph just for table
 from tabulate import tabulate
 
@@ -17,7 +18,18 @@ from tabulate import tabulate
 # P followed by Var letter is Prefix, Eg milli-volts
 
 # Initiating Lists
-table = [['Time (s)', 'Voltage difference', 'Capacitor Voltage', 'Capacitance(F)', 'Resistance', 'Current(A)', 'Charge moved (C)', 'Total Charge (C)']]
+table = [
+    [
+        "Time (s)",
+        "Voltage difference",
+        "Capacitor Voltage",
+        "Capacitance(F)",
+        "Resistance",
+        "Current(A)",
+        "Charge moved (C)",
+        "Total Charge (C)",
+    ]
+]
 graphs1 = []
 graphs2 = []
 # Initiating Values
@@ -31,97 +43,99 @@ R = 0
 IC = 0
 
 # Inputs
-PS = input('Prefix for Voltage - kilo, mega, giga \nmilli, micro (µ), nano, volts:')
+PS = input("Prefix for Voltage - kilo, mega, giga \nmilli, micro (µ), nano, volts:")
 PS = PS.lower()
 PS = PS.strip()
-if PS != 'volts':
-    S = input('Supply Voltage ('+ PS + 'volts):')
+if PS != "volts":
+    S = input("Supply Voltage (" + PS + "volts):")
     S = S.strip()
     S = float(S)
-if PS == 'volts':
-    S = input('Supply Voltage (V):')
+if PS == "volts":
+    S = input("Supply Voltage (V):")
     S = S.strip()
     S = float(S)
-if PS == 'kilo':
+if PS == "kilo":
     S = float(S)
     S = S * 10 ** 3
-if PS == 'mega':
+if PS == "mega":
     S = float(S)
     S = S * 10 ** 6
-if PS == 'giga':
+if PS == "giga":
     S = float(S)
     S = S * 10 ** 9
-if PS == 'milli':
+if PS == "milli":
     S = float(S)
     S = S * 10 ** -3
-if PS == 'micro (µ)':
+if PS == "micro (µ)":
     S = float(S)
     S = S * 10 ** -6
-if PS == 'nano':
+if PS == "nano":
     S = float(S)
     S = S * 10 ** -9
 
-PC = input('Prefix for Capacitance - kilo, mega, giga \nmilli, micro (µ), nano, pico, Farad:')
+PC = input(
+    "Prefix for Capacitance - kilo, mega, giga \nmilli, micro (µ), nano, pico, Farad:"
+)
 PC = PC.lower()
 PC = PC.strip()
-if PC != 'farad':
-    C = input('Capacitance ('+ PC + 'farad):')
+if PC != "farad":
+    C = input("Capacitance (" + PC + "farad):")
     C = C.strip()
     C = float(C)
-if PC == 'farad':
-    C = input('Capacitance (F)')
+if PC == "farad":
+    C = input("Capacitance (F)")
     C = C.strip()
     C = float(C)
-if PC == 'kilo':
+if PC == "kilo":
     C = float(C)
     C = C * 10 ** 3
-if PC == 'mega':
+if PC == "mega":
     C = float(C)
     C = C * 10 ** 6
-if PC == 'giga':
+if PC == "giga":
     C = float(C)
     C = C * 10 ** 9
-if PC == 'milli':
+if PC == "milli":
     C = float(C)
     C = C * 10 ** -3
-if PC == 'micro (µ)':
+if PC == "micro (µ)":
     C = float(C)
     C = C * 10 ** -6
-if PC == 'nano':
+if PC == "nano":
     C = float(C)
     C = C * 10 ** -9
     print(C)
-DT = input('Time Interval (S):')
+DT = input("Time Interval (S):")
 DT = DT.strip()
 DT = float(DT)
 
-PR = input('Prefix for Resistance - kilo, mega, giga \nmilli, micro (µ), nano, Ohms:')
+PR = input("Prefix for Resistance - kilo, mega, giga \nmilli, micro (µ), nano, Ohms:")
 PR = PR.lower()
 PR = PR.strip()
-if PR != 'ohms':
-    R = input('Resistance ('+ PR + 'ohms):')
+if PR != "ohms":
+    R = input("Resistance (" + PR + "ohms):")
     R = R.strip()
     R = float(R)
-if PR == 'ohms':
-    R = input('Resistance (V):')
+if PR == "ohms":
+    R = input("Resistance (V):")
     R = R.strip()
     R = float(R)
-if PR == 'kilo':
+if PR == "kilo":
     R = float(R)
     R = R * 10 ** 3
-if PR == 'mega':
+if PR == "mega":
     R = float(R)
     R = R * 10 ** 6
-if PR == 'giga':
+if PR == "giga":
     R = float(R)
     R = R * 10 ** 9
-if PR == 'milli':
+if PR == "milli":
     R = float(R)
     R = R * 10 ** -3
-if PR == 'micro (µ) (µ)':
+if PR == "micro (µ) (µ)":
     R = float(R)
     R = R * 10 ** -6
-if PR == 'nano':
+if PR == "nano":
     R = float(R)
     R = R * 10 ** -9
 
@@ -130,11 +144,13 @@ RC = R * C
 RT = RC * 5
 RT = int(RT)
 
+
 def tabling():
     global T, VD, CV, C, R, IC, CM, Q
     table.append([T, VD, CV, C, R, IC, CM, Q])
     graphs1.append(T)
     graphs2.append(VD)
+
 
 # While Less that Time Constant
 while T <= RT:
@@ -149,16 +165,14 @@ while T <= RT:
     # the current changes the amount of charge carried on to the capacitor in the time interval
     DQ = IC * DT
     Q = Q + DQ
-    #Calls Function
+    # Calls Function
     # Function so it can be callable
     tabling()
 
 
 print(tabulate(table))
 plt.plot(graphs1, graphs2)
-plt.xlabel('Time (s)')
-plt.ylabel('Voltage Diff (V)')
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage Diff (V)")
 
 plt.show()
-
-
